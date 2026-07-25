ec01bcc40120dc066ebed671e08f7a082c1378ee
ming <it.work.zhang@foxmail.com>
2026-07-25 09:30:54 +0800

    调整 移除device_info_plus依赖

--- lib/src/editor/toolbar/mobile/utils/keyboard_height_observer.dart
+++ lib/src/editor/toolbar/mobile/utils/keyboard_height_observer.dart
@@ -1,5 +1,4 @@
 import 'package:appflowy_editor/src/editor/util/platform_extension.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:keyboard_height_plugin/keyboard_height_plugin.dart';
 
 typedef KeyboardHeightCallback = void Function(double height);
@@ -7,14 +6,11 @@ typedef KeyboardHeightCallback = void Function(double height);
 // the KeyboardHeightPlugin only accepts one listener, so we need to create a
 //  singleton class to manage the multiple listeners.
 class KeyboardHeightObserver {
-  static int androidSDKVersion = -1;
-
   KeyboardHeightObserver._() {
-    if (PlatformExtension.isAndroid && androidSDKVersion == -1) {
-      DeviceInfoPlugin().androidInfo.then(
-            (value) => androidSDKVersion = value.version.sdkInt,
-          );
-    }
+    // 注：原实现会通过 device_info_plus 读取 Android SDK 版本写入静态字段，
+    // 但全工程无任何读取方，且 device_info_plus 在 Android 上会反射访问
+    // Build.SERIAL 等设备标识符，引发应用商店 SN 合规审核驳回。
+    // 此处移除该无用采集，并从 pubspec 中删除 device_info_plus 依赖。
     _keyboardHeightPlugin.onKeyboardHeightChanged((height) {
       notify(height);
 
